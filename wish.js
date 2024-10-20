36a1775977a89be16e57031afe0a0487ce0c4e17
Bri <45642328+brichua@users.noreply.github.com>
2024-10-20 03:40:06 -0400

    star!

--- wish.js
+++ wish.js
@@ -15,22 +15,12 @@ async function loadWishes(){
         const { x, y } = getRandomPosition(1500, 400, 50, 50);
         img.style.left = `${x}px`;
         img.style.top = `${y}px`;
+        img.id = "star" + i;
 
-        img.addEventListener('click', () => receiveWish(i));
+        (function(index) {
+            img.addEventListener('click', () => receiveWish(index));
+        })(i);
 
         document.getElementById('wishStar').appendChild(img);
     }
-}
-
-async function receiveWish(){
-    var stars = await db.collection("Wishes").doc('allWishes').get().then(doc => doc.get('wish'));
-    const randomIndex = Math.floor(Math.random() * stars.length);
-    console.log(randomIndex );
-    console.log(stars[randomIndex].content);
-    if(!imageVisable){
-        const message = document.getElementById('wishMessage');
-        message.classList.add('slide-up');
-    }
-
-
 }
